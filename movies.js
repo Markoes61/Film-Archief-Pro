13d9f53bded38dc534802c5851bc51489d335711
Markoes61 <majelings@gmail.com>
2026-03-28 14:56:07 +0100

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,20 +1,714 @@
 const initialMovies = [
-    { title: "Spider-Man: No Way Home", rating: 8 },
-    { title: "Going in Style", rating: 7.5 },
-    { title: "The Shining", rating: 7.5 },
-    { title: "Cop Land", rating: 7.5 },
-    { title: "The Beekeeper", rating: 7.5 },
-    { title: "Taken 2", rating: 7.5 },
-    { title: "Widows", rating: 7.5 },
-    { title: "The Call", rating: 7.5 },
-    { title: "Cabin in the Woods", rating: 7.5 },
-    { title: "Aftermath", rating: 7.5 },
-    { title: "Fatal Affair", rating: 7.5 },
-    { title: "The Cloverfield Paradox", rating: 7.5 },
-    { title: "Kiss the Girls", rating: 7.5 },
-    { title: "Final Destination 3", rating: 7.5 },
-    { title: "Final Destination 4", rating: 7.5 },
-    { title: "Final Destination 5", rating: 7.5 },
-    { title: "Rocky 5", rating: 7.5 },
-    { title: "The Investigation of Lucy Letby", rating: 7.5 }
+Breaking Bad - 10
+Enemy of the State - 10
+The Avengers - 10
+Fracture - 10
+A Perfect Murder - 10
+The Good, the Bad and the Ugly - 10
+Jackie Brown - 10
+Pulp Fiction - 10
+Avengers: Endgame - 10
+Breaking Bad - 10
+Donnie Brasco - 9.5
+Avengers: Infinity War - 9.5
+Avengers: Age of Ultron - 9.5
+Jackie Brown - 9.5
+I Am Legend - 9.5
+Frailty - 9.5
+Once upon a time in the west - 9.5
+Django Unchained - 9.5
+Storm of the Century Stephen King - 9.5
+Misery - 9.5
+Escape From Alcatraz - 9.5
+The usual suspects - 9.5
+Mistig river - 9.5
+Mystic River - 9.5
+Sleepers - 9.5
+Prison Break - 9.5
+At Close Range - 9.5
+prison break - 9
+The Boys from Brazil - 9
+Full Metal Jacket - 9
+Goodfellas - 9
+Casino - 9
+Carlito's Way - 9
+The Dark Knight - 9
+The Dark Knight Rises - 9
+The Silence of the Lambs - 9
+Seven - 9
+The Sixth Sense - 9
+Training Day - 9
+A Few Good Men - 9
+World War Z - 9
+12 Monkeys - 9
+Monster - 9
+Narcos - 9
+Unfaithful - 9
+2012 - 9
+Once Upon a Time in the West - 9
+Unforgiven - 9
+Man on Fire - 9
+Shooter - 9
+Déjà Vu - 9
+The Game - 9
+Primal Fear - 9
+Falling Down - 9
+Rocky - 9
+Rocky II - 9
+First Blood - 9
+Rambo: Last Blood (2019) - 9
+the last of us - 9
+No Country for Old Men - 9
+One flew over the co - 9
+Inglourious Basterds - 9
+Dolores Claiborne - 9
+Reservoir Dogs - 9
+Daddy's Home - 9
+Daddy's home 2 - 9
+Wild Hogs - 9
+Sicario 2 - 9
+Sicario Day of the Soldado - 9
+Logan - 9
+Iron Man - 9
+Guardians of the Galaxy - 9
+Guardians of the Galaxy Vol. 2 - 9
+Guardians of the Galaxy Vol. 3 - 9
+Absolute Power - 9
+Blood diamant - 9
+Walking with Dinosaurs - 9
+Walking with Beasts - 9
+Walking with Cavemen - 9
+To Catch a Killer - 9
+Shutter Island - 9
+Narcos - 9
+Mississippi Burning - 9
+Edge of Tomorrow - 9
+Breakdown - 9
+X-Men Origins: Wolverine - 9
+The Wolverine - 9
+The Ten Commandments - 9
+The Bourne Identity - 9
+Guy Ritchie's The Covenant - 9
+The Deer Hunter - 9
+Back to the Future - 9
+The Guns of Navarone - 9
+Force 10 from Navarone - 9
+Doctor Strange - 9
+Doctor Strange in the Multiverse of Madness - 9
+Easy Rider - 8.5
+Deep Cover - 8.5
+Collateral - 8.5
+L.A. Confidential - 8.5
+One Flew Over the Cuckoo's Nest - 8.5
+The Godfather Deel 1 - 8.5
+The Godfather Deel 2 - 8.5
+Once Upon a Time in America - 8.5
+Scarface - 8.5
+Law Abiding Citizen - 8.5
+A Bronx Tale - 8.5
+Anger Management - 8.5
+The Fugitive - 8.5
+Heat - 8.5
+American Gangster - 8.5
+Kill Bill Vol. 1 - 8.5
+Kill Bill Vol. 2 - 8.5
+Rambo: Last Blood - 8.5
+The Others - 8.5
+Flightplan - 8.5
+Gone Baby Gone - 8.5
+Case 39 - 8.5
+Gothika - 8.5
+10 Cloverfield Lane - 8.5
+Perfect Stranger - 8.5
+Bridge of Spies - 8.5
+Escobar: Paradise Lost - 8.5
+Gone Girl - 8.5
+Unbreakable - 8.5
+A Quiet Place: Day One - 8.5
+The Omen 1 - 8.5
+The Omen 2 - 8.5
+The Omen 3 - 8.5
+Absolute Power - 8.5
+The Terminator - 8.5
+16 Blocks - 8.5
+28 Weeks Later - 8.5
+The Day the Earth Stood Still - 8.5
+Disclosure - 8.5
+Rocky Balboa - 8.5
+Rocky IV - 8.5
+Rocky III - 8.5
+Planet of the Apes (1968) - 8.5
+Narcos - 8.5
+Grand Tour - 8.5
+True Romance - 8.5
+The Green Mile - 8.5
+The Shawshank Redemption - 8.5
+The Deliberate Stranger - 8.5
+french connection - 8.5
+the french connection 2 - 8.5
+No Way Out (1987) - 8.5
+Anger Management - 8.5
+Prisoners - 8.5
+Hulk - 8.5
+the incredible hulk - 8.5
+End of Days - 8.5
+The Fugitive - 8.5
+Warrior - 8.5
+Extremely Wicked, Shockingly Evil - 8.5
+No Time to Die - 8.5
+Casino Royale - 8.5
+Quantum of Solace - 8.5
+Skyfall - 8.5
+Spectre - 8.5
+Hancock - 8.5
+Jumanji: Welcome to the Jungle - 8.5
+​Jumanji: The Next Level - 8.5
+The Legend of Tarzan - 8.5
+True Story - 8.5
+City of God - 8.5
+Columbo - 8.5
+Narcos: Mexico - 8.5
+Gangs of New York - 8.5
+The Hateful Eight - 8.5
+Source Code - 8.5
+X-Men - 8.5
+X-Men 2 - 8.5
+X-Men: The Last Stand - 8.5
+X-Men: First Class - 8.5
+X-Men: Days of Future Past - 8.5
+X-Men: Apocalypse - 8.5
+X-Men: Dark Phoenix - 8.5
+Next - 8.5
+The Next Three Days - 8.5
+The Bourne Supremacy - 8.5
+The Bourne Ultimatum - 8.5
+The Juror - 8.5
+Extraction - 8.5
+Extraction 2 - 8.5
+King of New York - 8.5
+The Dead Zone - 8.5
+Back to the Future Part II - 8.5
+Back to the Future Part III - 8.5
+National Lampoon's Christmas Vacation - 8.5
+Street Kings - 8.5
+We're the Millers - 8.5
+Two and a Half Men - 8.5
+Arlington Road - 8.5
+Platoon - 8.5
+We Were Soldiers - 8.5
+Heartbreak Ridge - 8.5
+Casualties of War - 8.5
+Thor - 8.5
+Shang - Chi and the Legend of the Ten Rings - 8.5
+Thor: Love and Thunder - 8.5
+Deadpool & Wolverine - 8.5
+The Grey - 8
+Iron Man 2 - 8
+Iron Man 3 - 8
+Equalizer 1 - 8
+Land of the Dead - 8
+Knock Knock - 8
+Final Destination 1 - 8
+Final Destination 2 - 8
+The Silencing - 8
+Kidnap - 8
+Invisible Man - 8
+The Missing - 8
+Love and Monsters - 8
+The Talented Mr. Ripley - 8
+Evil Influencer: The Jodi Hildebrandt Story - 8
+Back in Action - 8
+Big Beasts: Last of the Giants - 8
+Cosmos: A Spacetime Odyssey - 8
+The Green Mile - 8
+Wolverine Trilogie - 8
+The Grey - 8
+Taken 1 - 8
+Iron Man 2 - 8
+Iron Man 3 - 8
+Land of the Dead - 8
+Knock Knock - 8
+Final Destination 1 - 8
+Final Destination 2 - 8
+The Silencing - 8
+Kidnap - 8
+Invisible Man - 8
+The Missing - 8
+Love and Monsters - 8
+The Talented Mr. Ripley - 8
+Evil Influencer: The Jodi Hildebrandt Story - 8
+Back in Action - 8
+Waterworld - 8
+What Women Want - 8
+Knock at the Cabin - 8
+Die Hard 1 - 8
+The Accountant 1 - 8
+The Accountant 2 - 8
+What Lies Beneath - 8
+Point Break - 8
+Sleeping Dogs - 8
+Unhinged - 8
+Mad Max 1 - 8
+Mad Max 2 - 8
+Mad Max 3 - 8
+Mission Impossible - 8
+A Quiet Place - 8
+47 Ronin - 8
+The Batman - 8
+Sniper - 8
+Poseidon - 8
+El Dorado - 8
+True Crime - 8
+Dances with Wolves - 8
+Star Wars: Originele Trilogie - 8
+The Revenant - 8
+Terminator 2: Judgment Day - 8
+Terminator 3: Rise of the Machines - 8
+JFK - 8
+Exodus: Gods and Kings - 8
+Indiana Jones - 8
+White Men Can't Jump - 8
+Surrogates - 8
+The Lost City - 8
+San Andreas - 8
+Rampage - 8
+The Book of Eli - 8
+Inside Man - 8
+The Little Things - 8
+Flight - 8
+Malcolm X - 8
+The Hurricane - 8
+True Story - 8
+Forensic Files - 8
+Dreamcatcher - 8
+28 Days Later - 8
+28 years later 2 - 8
+28 years later 1 - 8
+The War of the Worlds (2005) - 8
+Wall Street (1987) - 8
+Rise of the Planet of the Apes - 8
+Dawn of the Planet of the Apes - 8
+War for the Planet of the Apes - 8
+Narcos Mexico - 8
+Top Gear - 8
+The Big Bang Theory - 8
+Dahmer Monster - 8
+Enter the Dragon - 8
+The Way of the Dragon - 8
+The Hateful Eight - 8
+Doctor Sleep - 8
+The Fourth Protocol - 8
+Predator - 8
+Quigley Down Under - 8
+In the Line of Fire - 8
+Gran Torino - 8
+Legionnaire - 8
+Game of Death - 8
+Dr. No - 8
+From Russia with Love - 8
+Goldfinger - 8
+Thunderball - 8
+You Only Live Twice - 8
+Diamonds Are Forever - 8
+Live and Let Die - 8
+The Man with the Golden Gun - 8
+The Spy Who Loved Me - 8
+Moonraker - 8
+For Your Eyes Only - 8
+Octopussy - 8
+Never Say Never Again - 8
+A View to a Kill - 8
+The Living Daylights - 8
+Licence to Kill - 8
+GoldenEye - 8
+Tomorrow Never Dies - 8
+The World Is Not Enough - 8
+Die Another Day - 8
+Four brothers - 8
+Gang Related - 8
+Independence Day - 8
+Prehistoric Planet - 8
+Cosmos - 8
+Black Summer - 8
+Power Book III: Raising Kanan - 8
+Miami Vice - 8
+It - 8
+Wind River - 8
+Road House - 8
+The Walking Dead - 8
+The Crazies - 8
+Lake Fear - 8
+John Wick - 8
+El Chapo - 8
+Ant-Man - 8
+Ant-Man and the Wasp - 8
+Ant-Man and the Wasp: Quantumania - 8
+Payback - 8
+Ransom - 8
+Runaway Jury - 8
+Deadpool - 8
+Ghost Rider - 8
+Ghost Rider: Spirit of Vengeance - 8
+The Bourne Legacy - 8
+The Prophecy - 8
+The Prophecy II - 8
+The Prophecy 3: The Ascent - 8
+Ronin - 8
+Midnight Run - 8
+Dirty Rotten Scoundrels - 8
+The Big Bang Theory - 8
+Wall Street - 8
+Anger Management - 8
+Bad Boys - 8
+Bad Boys: Ride or Die - 8
+Shaft - 8
+I, Robot - 8
+The Outlaw Josey Wales - 8
+Pale Rider - 8
+For a Few Dollars More - 8
+A Fistful of Dollars - 8
+A Nightmare on Elm Street - 8
+A Nightmare on Elm Street - 8
+Rush Hour - 8
+Rush Hour 3 - 8
+The Thing - 8
+Captain America: The First Avenger - 8
+Thor: The Dark World - 8
+Captain America: The Winter Soldier - 8
+Captain America: Civil War - 8
+Spider - Man: Homecoming - 8
+Thor: Ragnarok - 8
+Captain Marvel - 8
+Spider - Man: Far From Home - 8
+Black Widow - 8
+Spider - Man: No Way Home - 8
+Going in Style - 7.5
+The Shining - 7.5
+Cop Land - 7.5
+The Beekeeper - 7.5
+Taken 2 - 7.5
+Widows - 7.5
+The Call - 7.5
+Cabin in the Woods - 7.5
+Aftermath - 7.5
+Fatal Affair - 7.5
+The Cloverfield Paradox - 7.5
+Kiss the Girls - 7.5
+Final Destination 3 - 7.5
+Final Destination 4 - 7.5
+Final Destination 5 - 7.5
+Rocky 5 - 7.5
+The Investigation of Lucy Letby - 7.5
+Havoc - 7.5
+Ferry 2 - 7.5
+Undercover - 7.5
+Sweet Girl - 7.5
+Aquaman 1 - 7.5
+Aquaman 2 - 7.5
+The Gray Man - 7.5
+Murder Mystery 2 - 7.5
+Play Dirty - 7.5
+Heads of State - 7.5
+Die Hard - 7.5
+Guns Up - 7.5
+Condemned - 7.5
+Someone to Watch Over Me - 7.5
+Brokeback Mountain - 7.5
+Mr. Brooks - 7.5
+How the Universe Works - 7.5
+Star Wars - 7.5
+Terminator Salvation - 7.5
+Night at the Museum - 7.5
+Race to Witch Mountain - 7.5
+Jungle Cruise - 7.5
+The Untouchables - 7.5
+The Departed - 7.5
+Cosmos - 7.5
+IT: Chapter One - 7.5
+IT: Chapter Two. - 7.5
+Fatal Attraction - 7.5
+Basic Instinct - 7.5
+Rambo: First Blood Part II - 7.5
+Kingdom of the Planet of the Apes - 7.5
+El Chapo - 7.5
+Vis a Vis - 7.5
+Miami Vice - 7.5
+Miami Vice - 7.5
+Soldier - 7.5
+11.22.63 - 7.5
+The Mist - 7.5
+Prison Break the final break - 7.5
+Philip Marlowe, Private Eye - 7.5
+Perry Mason: The Case of the - 7.5
+Case of the Notorious Nun - 7.5
+The Case of the Killer Kiss - 7.5
+An Innocent Man - 7.5
+Dirty Harry - 7.5
+Magnum Force - 7.5
+The Enforcer - 7.5
+The Dead Pool - 7.5
+Hard Target - 7.5
+Replicant - 7.5
+In Hell - 7.5
+Wake of Death - 7.5
+Until Death - 7.5
+Baywatch - 7.5
+The stand stephen King - 7.5
+New jack city - 7.5
+De aanslag - 7.5
+Jumanji - 7.5
+Bad Influence - 7.5
+How the Universe Works - 7.5
+Sea Monsters - 7.5
+Stranger Things - 7.5
+Mocro Maffia - 7.5
+Mocro Maffia: Tatta - 7.5
+Snowfall - 7.5
+Forensic Files - 7.5
+Undercover - 7.5
+Ferry - 7.5
+Ferry 2 - 7.5
+Miami Vice - 7.5
+Léon - 7.5
+Road House - 7.5
+Prey - 7.5
+Prey24 - 7.5
+Planes, Trains and Automobiles - 7.5
+Pablo Escobar, The Drug Lord - 7.5
+The Kid - 7.5
+Innerspace - 7.5
+Die Hard: With a Vengeance - 7.5
+Die Hard 4.0 - 7.5
+A Good Day to Die Hard - 7.5
+Plane - 7.5
+Identity - 7.5
+Deepwater Horizon - 7.5
+Deadpool 2 - 7.5
+Next of Kin - 7.5
+Jason Bourne - 7.5
+Vacation - 7.5
+National Lampoon's Vacation - 7.5
+National Lampoon's European Vacation - 7.5
+50 First Dates - 7.5
+Bad Boys II - 7.5
+Bad Boys For Life - 7.5
+Every Which Way but Loose - 7.5
+Any Which Way You Can - 7.5
+Shaft - 7.5
+Rich Man, Poor Man - 7.5
+Crime 101 - 7.5
+Mindhunters - 7.5
+Haunted Mansion - 7.5
+Gran Turismo - 7.5
+The Running Man - 7.5
+The Running Man - 7.5
+Freddy's Dead: The Final Nightmare (1991) - 7.5
+Wes Craven's New Nightmare (1994) - 7.5
+A Nightmare on Elm Street Part 2: Freddy's Revenge - 7.5
+A Nightmare on Elm Street: The Dream Child - 7.5
+A Nightmare on Elm Street 4: The Dream Master - 7.5
+A Nightmare on Elm Street Part 2: Freddy's Revenge - 7.5
+A Nightmare on Elm Street 3: Dream Warriors - 7.5
+Friday the 13th - 7.5
+Freddy vs. Jason - 7.5
+Friday - 7.5
+Next Friday - 7.5
+Friday After Next - 7.5
+Rush Hour 2 - 7.5
+K-9 - 7.5
+Turner & Hooch - 7.5
+Black Panther - 7.5
+Black Panther: Wakanda Forever - 7.5
+The Italian Job - 7.5
+The Bank Job - 7.5
+Mechanic: Resurrection - 7.5
+Shelter - 7.5
+powerbook - 7
+powerbook - 7
+Powerbook I - 7
+power book - 7
+El Camino: A Breaking Bad Movie - 7
+Ricochet - 7
+The Town - 7
+Cape Fear - 7
+Wrath of Man - 7
+Taken 3 - 7
+Rambo 2 - 7
+Rambo 3 - 7
+Rambo 4 - 7
+Krampus - 7
+The Tuxedo - 7
+Honest Thief - 7
+Tron - 7
+Tron: Legacy - 7
+What Men Want - 7
+Shotgun Wedding - 7
+The Last Dragon - 7
+The Alto Knights - 7
+The Family Plan 1 - 7
+The Family Plan 2 - 7
+Freelance - 7
+The Meg - 7
+The Rock - 7
+How the Universe Works - 7
+Langoliers - 7
+Traffic - 7
+Rambo III (1988) - 7
+Rambo (2008) - 7
+Beneath the Planet of the Apes (1970) - 7
+Beneath the Planet of the Apes - 7
+Escape from the Planet of the Apes - 7
+Planet of the Apes (2001) - 7
+Undercover - 7
+Ferry - 7
+Ferry 2 - 7
+The Big Boss - 7
+Fist of Fury - 7
+Django - 7
+matlock - 7
+Bloodsport - 7
+Kickboxer - 7
+Lionheart - 7
+Universal Soldier - 7
+Double Impact - 7
+Timecop - 7
+Sudden Impact - 7
+Maximum Risk - 7
+Knock Of - 7
+Inferno - 7
+The Hard Corps - 7
+Thunderball 8 - 7
+On Her Majesty's Secret Service - 7
+Independence Day: Resurgence - 7
+​Black Adam - 7
+Sleepers - 7
+Mocro Maffia: Komtgoed - 7
+Mocro Maffia: Taxi - 7
+Crime Story - 7
+Ozark - 7
+Fantasy Island - 7
+Fantasy Island - 7
+The Burbs - 7
+Planes, Trains and Automobiles - 7
+Blade - 7
+Lethal Weapon - 7
+Lethal Weapon 2 - 7
+Lethal Weapon 3 - 7
+Lethal Weapon 4 - 7
+Die Hard 2 - 7
+The Nice Guys - 7
+Romancing the Stone - 7
+The Jewel of the Nile - 7
+The Hard Way - 7
+Fletch - 7
+Fletch Lives - 7
+Vegas Vacation - 7
+The Wolf of Wall Street - 7
+Bad Boys - 7
+Shaft - 7
+Silver Streak Expresse - 7
+Stir Crazy - 7
+Haunted Honeymoon - 7
+The Haunted Mansion - 7
+Cool Runnings - 7
+Uncle Buck - 7
+Halloween - 7
+Halloween II - 7
+Halloween III: Season of the Witch - 7
+Halloween 4: The Return of Michael Myers - 7
+Halloween 5: The Revenge of Michael Myers - 7
+Halloween: The Curse of Michael Myers - 7
+Halloween H20: 20 Years Later - 7
+Halloween: Resurrection - 7
+Friday the 13th Part 2 - 7
+Friday the 13th Part III - 7
+Friday the 13th: A New Beginning - 7
+Friday the 13th Part VI: Jason Lives - 7
+Friday the 13th Part VII: The New Blood - 7
+Friday the 13th Part VIII: Jason Takes Manhattan - 7
+Jason Goes to Hell: The Final Friday - 7
+Jason X - 7
+Friday the 13th - 7
+K-911 - 7
+Eternals - 7
+The One - 7
+Mean Machine - 7
+The Transporter - 7
+Cellular - 7
+Transporter 2 - 7
+Revolver - 7
+London - 7
+Chaos - 7
+Crank - 7
+War - 7
+In the Name of the King - 7
+Transporter 3 - 7
+Crank: High Voltage - 7
+13 - 7
+The Mechanic - 7
+Blitz - 7
+Killer Elite - 7
+Safe - 7
+Parker - 7
+Homefront - 7
+Wild Card - 7
+Furious 7 - 7
+Spy - 7
+The Meg - 7
+Wrath of Man - 7
+Meg 2: The Trench - 7
+Operation Fortune: Ruse de Guerre - 7
+Expend4bles - 7
+The Expendables 3 - 7
+The Expendables 2 - 7
+The Expendables - 7
+A Working Man - 7
+Joker - 6.5
+Conquest of the Planet of the Apes - 6.5
+Battle for the Planet of the Apes - 6.5
+We Bury the Dead - 6.5
+The Green Hornet - 6.5
+Crime Story - 6.5
+Once Upon a Time in Hollywood - 6.5
+Baretta - 6.5
+Stuber - 6.5
+24 - 6.5
+Loki - 6.5
+WandaVision - 6.5
+Wiseguy - 6.5
+Wiseguy: Pilot - 6.5
+Baretta - 6.5
+The Passion of the Christ - 6.5
+Brewster's Millions - 6.5
+K-9: P.I. - 6.5
+Turn It Up - 6.5
+Ghosts of Mars - 6.5
+Death Race - 6.5
+Top Boy - 6
+Death Proof - 6
+Better call saul - 6
+The prestige - 6
+The Sopranos - 6
+The New Mutants - 6
+Cleopatra Jones - 6
+The Girl with the Dragon Tattoo - 6
+The Hunger Games - 6
+Air Force One - 6
+Beast - 6
+In Time - 6
+The Irishman - 6
+The Fate of the Furious - 5.5
+Fast X - 5.5
+Fast & Furious Presents: Hobbs & Shaw - 5.5
+Sniper - 5
+Meteor Moon - 5
+John Wick 2 - 5
+John Wick 3 - 5
+John Wick 4 - 5
+Marlowe - 5
+Lost - 5
+Aladdin - 5
+Monkey Man - 5
+GI Jane - 4.5
+22 Mile - 4
 ];
